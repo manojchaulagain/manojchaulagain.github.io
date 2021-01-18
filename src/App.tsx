c2c8edd229a4783e2cb616336896a969c1ee96a0
Manoj Chaulagain <machaulagain@sbgtv.com>
2021-01-17 21:02:04 -0800

    Project Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload and got created.
+          Final Edit <code>src/App.tsx</code> and save to reload and got created.
         </p>
         <a
           className="App-link"
