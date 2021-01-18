046fdd9d57e68de3a473f903392f3b178272f489
Manoj Chaulagain <machaulagain@sbgtv.com>
2021-01-17 23:30:04 -0800

    Project Initial Setup

---
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./SoftwareSkill.css";
+import { skillsSection } from "../../portfolio";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function SoftwareSkill() {
+  return (
+    <div>
+      <div className="software-skills-main-div">
+        <ul className="dev-icons">
+          {skillsSection.softwareSkills.map((skills,i) => {            
+            return (
+              
+              <li key={i} className="software-skill-inline" name={skills.skillName}>
+                <FontAwesomeIcon icon={skills.fontAwesomeClassname}/>              
+                <p>{skills.skillName}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
